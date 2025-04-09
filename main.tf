b8c8361ca5de60aade3c4716a280aadf8ee2997b
vishnu1002 <vishnuramguru@gmail.com>
2025-04-09 06:24:47 +0000

    new code changes

--- main.tf
+++ main.tf
@@ -1,19 +1,16 @@
-# Terraform Config
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
 }
 
-# Provider Setup
 provider "aws" {
   region = "us-east-1"
 }
 
-# VPC
 resource "aws_vpc" "app_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -21,47 +18,77 @@ resource "aws_vpc" "app_vpc" {
   }
 }
 
-# Pubic Subnet
-resource "aws_subnet" "app_public_subnet" {
-  vpc_id = aws_vpc.app_vpc.id
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.app_vpc.id
   cidr_block = "10.0.0.0/24"
   tags = {
-    Name = "App_Public_Subnet"
+    Name = "Public_Subnet"
   }
 }
 
-# IG
-resource "aws_internet_gateway" "app_ig" {
+resource "aws_internet_gateway" "app_igw" {
   vpc_id = aws_vpc.app_vpc.id
   tags = {
-    Name = "App_IG"
+    Name = "App_IGW"
   }
 }
 
-# RT
-resource "aws_route_table" "app_public_rt" {
+resource "aws_route_table" "app_rt" {
   vpc_id = aws_vpc.app_vpc.id
+
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.app_ig.id
+    gateway_id = aws_internet_gateway.app_igw.id
   }
+
   tags = {
     Name = "App_RT"
   }
 }
 
-# Associate RT with Subnet
-resource "aws_route_table_association" "app_public_assoc" {
-  subnet_id = aws_subnet.app_public_subnet.id
-  route_table_id = aws_route_table.app_public_rt.id
+resource "aws_route_table_association" "app_rt_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.app_rt.id
 }
 
-# EC2
-resource "aws_instance" "app_ec2" {
-  ami = "ami-084568db4383264d4"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.app_public_subnet.id
+resource "aws_security_group" "app_sg" {
+  name   = "HTTP_SSH"
+  vpc_id = aws_vpc.app_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
-    Name = "Public_EC2"
+    Name = "App_SG"
+  }
+}
+
+resource "aws_instance" "app_instance" {
+  ami                         = "ami-084568db4383264d4" # Replace if needed
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.app_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "App_Instance"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "ec2_instance_type" {
-  type = string
-  default = "t2.micro"
-}
+# variable "ec2_instance_type" {
+#   type = string
+#   default = "t2.micro"
+# }
 
-variable "ec2_tag_name" {
-  type = string
-  default = "App_EC2"
-}
+# variable "ec2_tag_name" {
+#   type = string
+#   default = "App_EC2"
+# }
